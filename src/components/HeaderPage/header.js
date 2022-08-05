32ae574feab49fd4af0e652b6c6417f961778b92
Kumar Shoolam <kumarshoolam@gmail.com>
2022-08-05 15:13:43 +0530

    Modifications-03

--- src/components/HeaderPage/header.js
+++ src/components/HeaderPage/header.js
@@ -1,26 +1,16 @@
 import React, { useState } from "react";
-import '../../assets/scss/header.css';
-import search from '../../assets/images/search.png';
-import user from '../../assets/images/user.png';
+import "../../assets/scss/header.css";
+import search from "../../assets/images/search.png";
+import user from "../../assets/images/user.png";
 import LoginApp from "../SignInPage/LoginApp";
-import basket from '../../assets/images/shopping-bag.png';
-import '../../assets/css/aem-grid.css';
+import basket from "../../assets/images/shopping-bag.png";
+import "../../assets/css/aem-grid.css";
 import { NavLink } from "react-router-dom";
-// import Skeleton from 'react-loading-skeleton';
-
 const Header = (props) => {
   const [loading, setLoading] = useState(false);
   const Loading = () => {
-    return (
-      <>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-          <Skeleton height={350} />
-          <Skeleton height={350} />
-        </div>
-      </>
-    );
-  }
+    return <></>;
+  };
   const ShowProducts = () => {
     return (
       <header className="header">
@@ -28,17 +18,26 @@ const Header = (props) => {
           <div class="aem-GridColumn--phone--12">
             <div className="container">
               <div className="header__content">
-                <div className="header__logo" >
-                  <NavLink className="logoo" to="/"><span>VENIA</span></NavLink>
+                <div className="header__logo">
+                  <NavLink className="logoo" to="/">
+                    <span>VENIA</span>
+                  </NavLink>
                   {/* <NavLink className="logoo" to="/"><span>V</span><span>ENIA</span></NavLink> */}
-
                 </div>
                 <div className="header__categories">
                   <ul>
-                    <li className=""><NavLink to="/women">Women</NavLink></li>
-                    <li><NavLink to="/men" >Men</NavLink></li>
-                    <li><NavLink to="/smartGear" >Smart Gear</NavLink></li>
-                    <li><NavLink to="/accessories" >Accessories</NavLink></li>
+                    <li className="">
+                      <NavLink to="/women">Women</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/men">Men</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/smartGear">Smart Gear</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/accessories">Accessories</NavLink>
+                    </li>
                   </ul>
                 </div>
                 <div className="header__icons">
@@ -55,13 +54,15 @@ const Header = (props) => {
                     </li> */}
                     <li className="cartCount">
                       <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-                        <img src={basket} alt="basket-image" />{' '}
+                        <img src={basket} alt="basket-image" />{" "}
                         {props.countCartItems ? (
-                          <button className="btn btn-primary">{props.countCartItems}</button>
+                          <button className="btn btn-primary">
+                            {props.countCartItems}
+                          </button>
                         ) : (
-                          ''
+                          ""
                         )}
-                      </NavLink>{' '}
+                      </NavLink>{" "}
                     </li>
                   </ul>
                 </div>
@@ -71,17 +72,16 @@ const Header = (props) => {
         </div>
       </header>
     );
-  }
+  };
 
   return (
     <div className="headerHamburger">
       <div className="my-5 py-5">
         <div className="justify-content-center">
           {loading ? <Loading /> : <ShowProducts />}
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Header;
